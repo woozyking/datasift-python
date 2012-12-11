6b707cf04c457c791bd479e2afb20bbd4c031ed7
Runzhou Li (Leo) <runzhou@addictivemobility.com>
2012-12-11 10:53:22 -0500

    Output Build Log for testing Jenkins

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -7,4 +7,7 @@ from test_definition  import TestDefinition
 from test_push        import TestPush
 
 # Run the tests
-unittest.main()
+log_file = "build.log"
+f = open(log_file, "w")
+runner = unittest.TextTestRunner(f)
+unittest.main(testRunner=runner)
